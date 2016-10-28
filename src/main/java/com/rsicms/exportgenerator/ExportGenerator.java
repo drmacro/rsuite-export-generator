2852aac0f1149c0c7545dd5d427035b727c49009
Eliot Kimber <ekimber@contrext.com>
2016-10-28 10:08:44 -0500

    Getting of random words seems to work

--- src/main/java/com/rsicms/exportgenerator/ExportGenerator.java
+++ src/main/java/com/rsicms/exportgenerator/ExportGenerator.java
@@ -1,10 +1,14 @@
 package com.rsicms.exportgenerator;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
 import java.io.*;
 import java.nio.file.FileSystems;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -20,6 +24,31 @@ public class ExportGenerator
     static Log log = LogFactory.getLog(ExportGenerator.class);
     private final File outdir;
     int moid = 1000; // Start with MO 1000 so MO IDs look realistic.
+
+    static ArrayList<String> wordList = new ArrayList<String>();
+
+    static {
+        String wordsFilePath = "/resources/data-files/words.txt";
+        InputStream inStream = ExportGenerator.class.getResourceAsStream(wordsFilePath);
+        if (inStream == null) {
+            throw new RuntimeException("Failed to load resource " + wordsFilePath);
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.startsWith("#")) {
+                    line = reader.readLine();
+                    continue;
+                }
+                wordList.add(line.trim());
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected " + e.getClass().getSimpleName() +
+                    " reading words file: " + e.getMessage(), e);
+        }
+    }
 
     public ExportGenerator(File parametersFilepath) throws Exception {
         this.generationParameters = new GenerationParameters(parametersFilepath);
@@ -130,16 +159,36 @@ public class ExportGenerator
 
     private void makeManagedObject(File moDir, int moid) throws Exception {
         File contentXml = new File(moDir, "content.xml");
-        PrintWriter writer = new PrintWriter(
-                new BufferedWriter(
-                        new OutputStreamWriter(
-                                new BufferedOutputStream(
-                                        new FileOutputStream(contentXml)), "UTF-8")));
-
-        writer.write("<topic id=\"id" + moid + "\">\n");
-        writer.write("</topic>\n");
+        XMLStreamWriter writer = XMLOutputFactory.newInstance().
+                createXMLStreamWriter(new BufferedOutputStream(
+                        new FileOutputStream(contentXml)), "UTF-8");
+        writer.writeStartDocument();
+        writer.writeStartElement("topic");
+        writer.writeAttribute("id", "" + moid);
+        writer.writeStartElement("title");
+        writer.writeCharacters(getRandomWords(2,5));
+        writer.writeEndElement();
+        writer.writeEndElement();
+        writer.writeEndDocument();
         writer.flush();
         writer.close();
+    }
+
+    /**
+     *
+     * @param min
+     * @param max
+     * @return A string of words picked at random with no fewer than min and
+     * no more than max words.
+     */
+    public static String getRandomWords(int min, int max) {
+        int wordCount = ThreadLocalRandom.current().nextInt(min, max+1);
+        ArrayList<String> words = new ArrayList<String>();
+        for (int i = 0; i < wordCount; i++) {
+            int wordNdx = ThreadLocalRandom.current().nextInt(0, wordList.size());
+            words.add(wordList.get(wordNdx));
+        }
+        return StringUtils.join(words, " ");
     }
 
     private int nextMoId() {
