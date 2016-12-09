aec71521b11931af592a55fce5e9f1882f652c14
Eliot Kimber <ekimber@contrext.com>
2016-12-09 15:03:08 -0600

    Refactored to separate XML file generation from rest of MO generation

--- src/main/java/com/rsicms/exportgenerator/mogeneration/ManagedObjectGeneratorBase.java
+++ src/main/java/com/rsicms/exportgenerator/mogeneration/ManagedObjectGeneratorBase.java
@@ -2,6 +2,7 @@ package com.rsicms.exportgenerator.mogeneration;
 
 import com.rsicms.exportgenerator.GenerationParameters;
 import com.rsicms.exportgenerator.api.ManagedObjectGenerator;
+import com.rsicms.exportgenerator.api.MoType;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -131,6 +132,15 @@ public abstract class ManagedObjectGeneratorBase implements ManagedObjectGenerat
         return dir;
     }
 
+    /**
+     * Generate the file that holds the content for an XML managed object.
+     * @param moid
+     * @param resultFile
+     * @param title
+     * @throws Exception
+     */
+    protected abstract void makeXmlContent(int moid, File resultFile, String title) throws Exception;
+
     protected void makeResourceFileForXmlMo(File moDir,
                                           int moid,
                                           String title,
@@ -190,5 +200,85 @@ public abstract class ManagedObjectGeneratorBase implements ManagedObjectGenerat
             fos.close();
         }
 
+    }
+
+    public void generateManagedObjects() throws Exception {
+        /*
+         * The managed object structure is:
+         *
+         * rsuite.content/
+         *    managed-objects/
+         *      0/
+         *        {n}/ (directories with numbers between 0 and 99, not necessary consecutive)
+         *          {moid}/ (One or more, usually no more than 2)
+         *            {moid}.resource  (system metadata for the MO)
+         *            {moid}-{versionid}.xml (XML for a specific version of the MO)
+         *            content.xml      (XML for the latest version)
+         *      ...
+         *      999/ (1000 directories number 1-999
+         */
+
+        /*
+         * Since RSuite tries to spread the MOs evenly over the directories, doing
+         * a breadth-first traversal of the directories, rather than a depth-first.
+         */
+
+        File contentDir = new File(generationParameters.getOutputDirectory(),
+                "rsuite.content");
+        File mosDir = new File(contentDir, "managed-objects");
+        if (!mosDir.mkdirs()) {
+            throw new RuntimeException("Failed to create directory \"" + mosDir.getAbsolutePath() + "\"");
+        }
+
+        long progressCtr = 0;
+        long dotCtr = 0;
+        // First make the top 100 directories.
+        for (long i = 0; i < generationParameters.getMaxMoCount(); i++) {
+            File topDir = getRandomDir(0, 999, mosDir);
+            File childDir = getRandomDir(0,100, topDir);
+            int moid = nextMoId();
+            File moDir = new File(childDir, "" + moid + "");
+            if (!moDir.mkdirs()) {
+                throw new RuntimeException("Failed to create directory \"" + moDir.getAbsolutePath() + "\"");
+            }
+
+            makeManagedObject(moDir, moid, MoType.XML);
+
+            progressCtr++;
+            if (progressCtr % 100 == 0) {
+                System.out.print(".");
+                dotCtr++;
+                if (dotCtr >= 60) {
+                    System.out.print("\n");
+                    dotCtr = 0;
+                }
+            }
+        }
+        System.out.print("\n");
+
+
+    }
+
+    protected void makeManagedObject(File moDir, int moid, MoType moType) throws Exception {
+        File contentXml = new File(moDir, "content.xml");
+        String title = getRandomWords(2,5);
+        makeXmlContent(moid, contentXml, title);
+        ArrayList<String> versionSpecs = new ArrayList<String>();
+        int majorVer = 1;
+        int minorVer = 0;
+        int versionCount = ThreadLocalRandom.current().nextInt(0, generationParameters.getMaxVersions() + 1);
+        for (int i = 0; i < versionCount; i++) {
+            if (i > 0 && i % 3 == 0) {
+                majorVer++;
+                minorVer = 0;
+            }
+            String versionSpec = majorVer + "." + minorVer;
+            minorVer++;
+            versionSpecs.add(versionSpec);
+            File verFile = new File(moDir, moid + "-" + versionSpec + ".xml");
+            makeXmlContent(moid, verFile, title);
+        }
+        makeResourceFileForXmlMo(moDir, moid, title, versionSpecs);
+        this.generationParameters.addMo(moid, moType);
     }
 }
--- src/test/java/com/rsicms/exportgenerator/mogeneration/TestManagedObjectGeneratorBase.java
+++ src/test/java/com/rsicms/exportgenerator/mogeneration/TestManagedObjectGeneratorBase.java
@@ -1,6 +1,7 @@
 package com.rsicms.exportgenerator.mogeneration;
 
 import com.rsicms.exportgenerator.GenerationParameters;
+import com.rsicms.exportgenerator.api.ManagedObjectGenerator;
 import com.rsicms.exportgenerator.api.MoType;
 import com.sun.xml.internal.ws.developer.UsesJAXBContext;
 import org.junit.Test;
@@ -19,14 +20,21 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TestManagedObjectGeneratorBase {
 
-    @Test
-    public void testCreateManagedObject() throws Exception {
-
+    private GenerationParameters getGenerationParameters() throws Exception {
         URL props = TestManagedObjectGeneratorBase.class.getResource("/generation.properties");
         assertNotNull("Didn't get props file", props);
         File propsFile = new File(props.getFile());
         GenerationParameters genParms = new GenerationParameters(propsFile);
-        genParms.setOutputDirectory(new File((String)(genParms.get("outdir"))));
+        File tempFile = File.createTempFile("rsi-", ".xml");
+        File outDir = tempFile.getParentFile();
+        genParms.setOutputDirectory(outDir);
+        return genParms;
+    }
+
+    @Test
+    public void testGenerationParametersMoHandling() throws Exception {
+
+        GenerationParameters genParms = getGenerationParameters();
         int moid = 1;
         genParms.addMo(moid, MoType.XML);
         ManagedObject mo = genParms.getManagedObject(moid);
@@ -35,7 +43,18 @@ public class TestManagedObjectGeneratorBase {
         ArrayList<ManagedObject> xmlMOs = genParms.getManagedObjectsOfType(MoType.XML);
         assertNotNull("Didn't get MO list back", xmlMOs);
         assertEquals("Expected 1 XML MO", xmlMOs.size(), 1);
+        genParms.getOutputDirectory().delete();
 
+    }
 
+    @Test
+    public void testCreateManagedObject() throws Exception {
+        GenerationParameters genParms = getGenerationParameters();
+        DitaManagedObjectGenerator moGenerator = new DitaManagedObjectGenerator(genParms);
+        int moid = moGenerator.nextMoId();
+        moGenerator.makeManagedObject(genParms.getOutputDirectory(), moid, MoType.XML);
+        ManagedObject mo = genParms.getManagedObject(moid);
+        assertNotNull(mo);
+        genParms.getOutputDirectory().delete();
     }
 }
